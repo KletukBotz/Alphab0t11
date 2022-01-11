7842eea32164ea166e4c22938b6a6f907588b5cd
KletukBotz <97545230+KletukBotz@users.noreply.github.com>
2022-01-12 01:41:35 +0700

    Update and rename shop/sewa.js to toko/sewa.js

--- toko/sewa.js
+++ toko/sewa.js
@@ -3,21 +3,20 @@ return `「 *PRICE LIST* 」
 							
 ⬣ PRICE SEWABOT
 
-⬡ 1 MINGGU = ${_minggu}
-⬡ 2 MINGGU = ${__minggu}
-⬡ 3 MINGGU = ${___minggu}
-⬡ 1 BULAN = ${_bulan}
-⬡ PERMANEN = ${_permanen}
-⬡ PERMANEN + USER PREM = ${__permanen}
+⬡ 1 MINGGU = 2000
+⬡ 2 MINGGU = 4000
+⬡ 3 MINGGU = 6000
+⬡ 1 BULAN = 10.000
+⬡ PERMANEN = 15.000
 
 
 ⬣ PRICE JADIBOT
 
-⬡ JADI BOT PERMANEN 25K
+⬡ JADI BOT PERMANEN 15.000
 
 
 ⬣ MINAT? PM
-⬡ wa.me/62887435047326
+⬡ wa.me/6285712157431
 
 
 NOTE : 
